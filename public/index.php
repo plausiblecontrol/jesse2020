0ab1700bfc5b67b89294d259d6f6281db1e6671f
Nick Andren <nick@seowarp.com>
2020-10-28 14:50:06 -0500

    Adding layout, text

--- public/index.php
+++ public/index.php
@@ -1,7 +1,50 @@
-<?php
-echo "
-    <center>
-        <div>
-            <img class=jesse src=\"/assets/jesse-pearson-2020.png\">
-        </div>
-    </center>";
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Jesse M. Pearson for President: Official Campaign Website
+        </title>
+        <link rel="stylesheet" href="/assets/presidential.css">
+    </head>
+    <body>
+        <header>
+            <ul>
+                <li><a class="active" href="/">Home</a></li>
+                <li><a href="/unofficial-sponsors.php">Unofficial Sponsors</a></li>
+            </ul>
+        </header>
+        <center>
+            <div>
+                <img class=jesse src="/assets/jesse-pearson-2020.png">
+            </div>
+            <div>
+                <h1>
+                    The 46th President of the United States<br />
+                    Jesse M. Pearson
+                </h1>
+                <p>
+                    <em>
+                        "GAMERS RISE UP!"
+                    </em>
+                    <br />
+                    <em>
+                        - Future President Jesse Pearson
+                    </em>
+                </p>
+
+                <p>
+                    Jesse Pearson is the very definition of the American success story, setting the standards of
+                    excellence in his speedrunning endeavors, and now, for the United States of America.
+                </p>
+
+                <p>
+                    A frequent shopper of Cub Foods, Future President Pearson has always dreamed big and pushed the
+                    boundaries of what is possible his entire career, devoting his life to building computers, roms
+                    and the American Dream. This was brought to life by a movement he inspired in the people of
+                    Nintendo of America when he announced his candidacy for President of the United States in
+                    October 2020.
+                </p>
+            </div>
+        </center>
+    </body>
+</html>
---
+++ public/unofficial-sponsors.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Unofficial Sponsors | Jesse M. Pearson for President: Official Campaign Website
+        </title>
+        <link rel="stylesheet" href="/assets/presidential.css">
+    </head>
+    <body>
+        <header>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a class="active" href="/unofficial-sponsors.php">Unofficial Sponsors</a></li>
+            </ul>
+        </header>
+        <center>
+            <div>
+                <img class=jesse src="/assets/jesse-pearson-2020.png">
+            </div>
+            <h1>
+                Unofficial Sponsors
+            </h1>
+            <div class="sponsors">
+                <ul>
+                    <li>
+                        <a href="https://www.nintendo.com/">
+                            Nintendo of America
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.cub.com/">
+                            Cub Foods (thx bae)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.gstatic.com/tv/thumb/persons/287557/287557_v9_bb.jpg">
+                            LeBron James
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://c1.staticflickr.com/1/810/27578450348_e2e80b0f04_n.jpg">
+                            Facebook
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </center>
+    </body>
+</html>
